37ef734ba2ac1433ca0442ac0b2164dfaab12e14
maheshreddi7 <maheshreddy.pannalaa@gmail.com>
2025-05-05 23:22:06 +0530

    meme generator

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function App(props) {
+    const [starWarsData, setStarWarsData] = React.useState(null)
+    
+    fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(data => setStarWarsData(data))
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}  
